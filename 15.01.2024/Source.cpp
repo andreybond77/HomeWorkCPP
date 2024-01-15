972750ccefe991778a26778b2e57ce3233eae3c4
Андрей <155187683+andreybond77@users.noreply.github.com>
2024-01-15 20:08:27 +0300

    Add files via upload

---
+++ 15.01.2024/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int age;
+public:
+    Student(string name, int age) {
+        this->age = age;
+        this->name = name;
+
+    }
+    void show() {
+count<<
+    }
+};
+
+
+
+
+
+
+int main() {
+   
+
+    return 0;
+}
