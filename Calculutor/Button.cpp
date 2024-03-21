fa9bbd8c515862c1695978df2360e1c6091f8817
Андрей <155187683+andreybond77@users.noreply.github.com>
2024-03-21 15:20:50 +0300

    Add files via upload

---
+++ Calculutor/Button.cpp
@@ -0,0 +1,55 @@
+#include "Button.h"
+Button::Button(sf::Vector2f size,
+	sf::Color color,
+	sf::Vector2f position,
+	std::string str)
+
+{
+	btnColor = color;
+	shape.setSize(size);
+	shape.setFillColor(color);
+	shape.setPosition(position);
+	text.setString(str);
+	text.setFillColor(sf::Color::White);
+	text.setPosition(sf::Vector2f(position.x + 20, position.y + 20));
+
+}
+
+void Button::draw(sf::RenderWindow& window)
+{
+	font.loadFromFile("arialbd.ttf");
+	text.setFont(font);
+	window.draw(shape);
+	window.draw(text);
+}
+
+sf::Vector2f Button::getPosition()
+{
+	return shape.getPosition();
+}
+
+sf::Vector2f Button::getSize()
+{
+	return shape.getSize();
+}
+
+void Button::changeActive()
+{
+	active = !active;
+}
+
+void Button::update()
+{
+	if (active) {
+		sf::Color color = shape.getFillColor();
+		shape.setFillColor(sf::Color(30, 30, 30));
+	}
+	else {
+		shape.setFillColor(btnColor);
+	}
+}
+
+std::string Button::getText()
+{
+	return text.getString();
+}
---
+++ Calculutor/ConsoleApplication13.cpp
@@ -0,0 +1,13 @@
+﻿
+
+#include <iostream>
+using namespace std;
+#include <SFML/Graphics.hpp>
+#include"Calculutor.h"
+
+int main()
+{
+    Calculator calculator;
+    calculator.run();
+}
+
